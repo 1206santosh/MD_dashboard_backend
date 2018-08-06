01b142dabcca2e8cf71af53ebfa4c3f26cd2bb24
1206santosh <santosh@linkwok.com>
2018-08-06 22:05:09 +0530

    rails_admin_import

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,6 @@
 class Meeting < ApplicationRecord
   has_many :tasks
-  belongs_to :created_by ,foreign_key: :created_by_id ,class_name: "User"
+  belongs_to :created_by ,foreign_key: :created_by_id ,class_name: "User",optional: :true
   has_many :user_meetings
   has_many :users ,through: :user_meetings
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,9 @@
 class Task < ApplicationRecord
-  belongs_to :meeting
+  belongs_to :meeting ,optional: :true
   has_many :uploads, as: :item
   has_many :comments ,as: :item
-  belongs_to :assignee ,foreign_key: :assignee_id,class_name:"User"
-  belongs_to :assigner ,foreign_key: :assigner_id,class_name:"User"
+  belongs_to :assignee ,foreign_key: :assignee_id,class_name:"User" ,optional: :true
+  belongs_to :assigner ,foreign_key: :assigner_id,class_name:"User",optional: :true
 
 
 
