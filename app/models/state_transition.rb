ee30f98a64c198028246533a977511de26bd4419
1206santosh <santosh@linkwok.com>
2018-08-14 21:16:38 +0530

    some changes

--- app/models/state_transition.rb
+++ app/models/state_transition.rb
@@ -1,4 +1,4 @@
 class StateTransition < ApplicationRecord
   belongs_to :item ,polymorphic: true ,optional: true
-  belongs_to :user ,foreign_key: :transition_by ,optional: true
+  belongs_to :transition_user ,foreign_key: :transition_by ,optional: true
 end
